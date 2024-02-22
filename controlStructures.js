730f77fbe9c9a2e76cc1065b7c62f6190fcf7f62
RingoDarius <44747519+RingoDarius@users.noreply.github.com>
2024-02-21 20:04:37 -0700

    Update controlStructures.js

--- controlStructures.js
+++ controlStructures.js
@@ -1,11 +1,4 @@
 document.getElementById("btnOne").onclick = function() {
 	var txt = document.getElementById("txtOne").value;
 	eval(txt);
-   /* var num = prompt("Enter a number");
-    if(num < 50){
-        alert(num + " is under 50");
-    }
-    else{
-        alert(num + " is above 50");
-    }*/
 };
