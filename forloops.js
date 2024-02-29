9ea893befce95c07551b334474d17c6dc318306b
RingoDarius <44747519+RingoDarius@users.noreply.github.com>
2024-02-28 20:24:26 -0700

    Nav completed

---
+++ forloops.js
@@ -0,0 +1,87 @@
+
+document.getElementById("btnThirteen").onclick = function() {
+
+	var txt = document.getElementById("txtThirteen").value;
+	eval(txt);
+	/*
+	var num = parseInt(prompt("Enter a number to count to"));
+	var sum = 0;
+	for(var i = 0; i < num; i++){
+		alert("Count: " + i);
+		sum += i;
+	}
+	alert("The sum of the numbers is " + sum);
+ */
+};
+
+document.getElementById("btnFourteen").onclick = function() {
+var txt = document.getElementById("txtFourteen").value;
+	eval(txt);
+	/*
+	var num = parseInt(prompt("Enter a number to count by:"));
+	var sum = 0;
+	for(var i = 0; i < 20; i += num){
+		alert("Count: " + i);
+		sum += i;
+	}
+	alert("The sum of the numbers is " + sum);
+ */
+};
+
+
+document.getElementById("btnFifteen").onclick = function() {
+var txt = document.getElementById("txtFifteen").value;
+	eval(txt);
+	/*for(var i = 10; i >= 0; i-=2){
+	alert("Count: " + i);
+}*/
+};
+
+
+document.getElementById("btnSixteen").onclick = function() {
+	var txt = document.getElementById("txtSixteen").value;
+	eval(txt);
+	/*var word = prompt("Enter a word");
+	for(var i = 0; i < word.length; i++){
+		var letter = word.charAt(i);
+		alert("Char At: " + letter);
+	}
+ */
+};
+
+document.getElementById("btnSeventeen").onclick = function() {
+	var txt = document.getElementById("txtSeventeen").value;
+	eval(txt);
+/*Write a script to allow the user to enter a word, it will show each of the letters in reverse order*/
+};
+
+
+document.getElementById("btnEighteen").onclick = function() {
+	var txt = document.getElementById("txtEighteen").value;
+	eval(txt);
+/*Write a script to allow the user to enter a number.  The script will then report back all the even numbers up to that number*/
+};
+
+
+	document.getElementById("btnNineteen").onclick = function() {
+		var txt = document.getElementById("txtNineteen").value;
+	eval(txt);
+		/*
+	var sentence = prompt("Enter a sentence");
+	var space = 0;
+	for(var i = 0; i < sentence.length; i++){
+		var letter = sentence.charAt(i);
+		if(letter == ' '){
+			space++;
+		}
+	}
+	alert("There are " + space  + " spaces in " + sentence);
+ */
+};
+
+
+document.getElementById("btnTwenty").onclick = function() {
+	var txt = document.getElementById("txtTwenty").value;
+	eval(txt);
+	/*Write a script to allow the user to enter a sentence: the program will report the number of vowels in that sentence */
+};
---
+++ nav.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+let links = [
+    {
+        "pageName": "Home",
+        "URL": "index.html"
+    },
+    {
+        "pageName": "Variables",
+        "URL": "variables.html"
+    },
+    {
+        "pageName": "If Statements",
+        "URL": "ifstatements.html"
+    },
+    {
+        "pageName": "While Loops",
+        "URL": "loops.html"
+    },
+    {
+        "pageName": "For Loops",
+        "URL": "forloops.html"
+    }
+];
+
+
+
+
+
+var mylist = document.createElement('ul');
+
+let txt = "";
+for(var i of links){
+    var atag = document.createElement('a');
+    atag.setAttribute("href", i.URL);
+    atag.textContent = i.pageName;
+    var liElementp = document.createElement('li');
+    liElementp.appendChild(atag);
+    mylist.appendChild(liElementp);
+  
+}
+
+
+document.getElementById("nav").appendChild(mylist);
